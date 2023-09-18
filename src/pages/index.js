972c994e70bd3cfc32cd1ff06b0cacaa9ffb178e
dalilia <dalila200875yahoo.fr>
2023-09-18 17:54:50 +0100

    feat(movie_page): creating movie page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,14 @@
 import Image from "next/image"
 import { Inter } from "next/font/google"
 import Head from "next/head"
-import Link from "next/link"
+
 
 const inter = Inter({ subsets: ["latin"] })
 
 export default function Homepage() {
   return (
     <div>
-      <Link href="/movies/569094">
-        Movies
-      </Link>
+      
     </div>
   )
 }
