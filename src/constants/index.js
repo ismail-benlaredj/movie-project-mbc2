cc66aa6cc4850e7f27fbc52446a5ceecb95230c9
ismailbenlaredj <yako_is@yahoo.fr>
2023-09-18 22:29:53 +0100

    refactor: use constant file in actors page (index file)

---
+++ src/constants/index.js
@@ -0,0 +1,6 @@
+const API_URL = "https://api.themoviedb.org"
+// ACTORS
+export const SINGLE_ACTOR = "https://api.themoviedb.org/3/person/",
+  MOVIES_BY_ACTOR = "/movie_credits",
+  POPULAR_ACTORS =
+    "https://api.themoviedb.org/3/person/popular?language=en-US&page=1"
